8cbadcc68bc859a1f9c9b750147ae7cb94e60541
Seweryn Czykinowski <sevveryn.czykinowski@gmail.com>
2025-05-30 14:56:30 +0200

    feat(shell): add basic shell

--- src/shell.rs
+++ src/shell.rs
@@ -3,3 +3,29 @@ use crate::fs::FileSystem;
 pub struct Shell {
     pub fs: FileSystem,
 }
+
+impl Shell {
+    pub fn new() -> Self {
+        Self {
+            fs: FileSystem::default(),
+        }
+    }
+}
+
+impl Default for Shell {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Shell {
+    pub fn execute(&self, command: &str) -> Result<(), String> {
+        if command.is_empty() {
+            return Err("Tried to execute an empty command".to_string());
+        }
+        // let tokens = shlex::split(command).ok_or("Failed to split command")?;
+        // let command = tokens.first().ok_or("Failed to get command")?;
+
+        Ok(())
+    }
+}
