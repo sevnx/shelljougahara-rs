14706a06fad8c2f8cf83ef629bdd99b42c6f220e
Seweryn Czykinowski <sevveryn.czykinowski@gmail.com>
2025-06-08 16:44:39 +0200

    feat(cd): implement cd command

--- src/commands/list/cd.rs
+++ src/commands/list/cd.rs
@@ -3,7 +3,7 @@
 use crate::{
     FileSystem,
     commands::{Command, CommandOutput, flags::Flags},
-    errors::ShellError,
+    errors::{FileSystemError, ShellError},
 };
 
 #[derive(Default)]
@@ -18,7 +18,22 @@ impl Command for ChangeDirectoryCommand {
         Flags::new()
     }
 
-    fn execute(&self, _: &[String], _: &mut FileSystem) -> Result<CommandOutput, ShellError> {
-        todo!()
+    fn execute(
+        &self,
+        args: &[String],
+        file_system: &mut FileSystem,
+    ) -> Result<CommandOutput, ShellError> {
+        let change_result = if args.is_empty() {
+            file_system.change_directory("~")
+        } else {
+            file_system.change_directory(&args[0])
+        };
+        match change_result {
+            Ok(_) => Ok(CommandOutput("".to_string())),
+            Err(ShellError::FileSystem(FileSystemError::DirectoryNotFound(message))) => Ok(
+                CommandOutput(format!("cd: {}: No such file or directory", message)),
+            ),
+            Err(e) => Err(e),
+        }
     }
 }
