76d08519e281e2ce8b1dc1aaff5edc9cfb0569c4
Seweryn Czykinowski <sevveryn.czykinowski@gmail.com>
2025-07-05 16:48:01 +0200

    test: add tests for touch

--- tests/shell/commands/touch.rs
+++ tests/shell/commands/touch.rs
@@ -35,3 +35,33 @@ fn test_touch_and_touch_again() {
         .expect("Failed to execute touch");
     assert_eq!(touch2.0, None);
 }
+
+#[test]
+fn test_touch_in_touched_file() {
+    let username = "test";
+    let mut shell = Shell::new_with_user(username);
+    let touch = shell
+        .execute("touch test")
+        .expect("Failed to execute touch");
+    assert_eq!(touch.0, None);
+    let touch2 = shell
+        .execute("touch test/test2")
+        .expect("Failed to execute touch");
+    assert_eq!(
+        touch2.0,
+        Some("touch: cannot touch 'test/test2': Not a directory".to_string())
+    );
+}
+
+#[test]
+fn test_touch_in_non_existent_directory() {
+    let username = "test";
+    let mut shell = Shell::new_with_user(username);
+    let touch = shell
+        .execute("touch test/test2")
+        .expect("Failed to execute touch");
+    assert_eq!(
+        touch.0,
+        Some("touch: cannot touch 'test/test2': No such file or directory".to_string())
+    );
+}
